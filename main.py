504bc033bcc6318f37dc894c72ef9e68b5f1a6cc
irinavladimirova462-spec <irinavladimirova462@gmail.com>
2026-01-04 18:18:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,35 @@ import io
 import requests
 from flask import Flask, request, send_file
 from pdf2image import convert_from_bytes
+import os
 
 app = Flask(__name__)
 
 @app.route('/render_card.jpg')
 def convert():
-    # Получаем ссылку на PDF из параметров запроса
     pdf_url = request.args.get('url')
     if not pdf_url:
-        return "Ошибка: Не указан URL", 400
+        return "URL is missing", 400
 
     try:
-        # Скачиваем PDF
-        response = requests.get(pdf_url, timeout=10)
-        # Конвертируем 1-ю страницу в картинку
+        response = requests.get(pdf_url, timeout=15)
+        response.raise_for_status()
+        
+        # Конвертируем PDF
         images = convert_from_bytes(response.content, dpi=200)
         
-        # Сохраняем результат в память (буфер)
+        if not images:
+            return "Could not convert PDF", 500
+
         img_io = io.BytesIO()
         images[0].save(img_io, 'JPEG', quality=85)
         img_io.seek(0)
         
         return send_file(img_io, mimetype='image/jpeg')
     except Exception as e:
-        return f"Ошибка конвертации: {str(e)}", 500
+        return str(e), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    # Render использует переменную окружения PORT
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
